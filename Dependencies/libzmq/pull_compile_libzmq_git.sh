8b344800277e5c6bb77ba0796352a5165b3a329e
Andres Botero <boterock@gmail.com>
2016-01-20 21:48:37 -0500

    Add diferent libzmq for ios and osx

--- Dependencies/libzmq/pull_compile_libzmq_git.sh
+++ Dependencies/libzmq/pull_compile_libzmq_git.sh
@@ -12,18 +12,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 GLOBAL_OUTDIR=${DIR}/dependencies
 BUILD_DIR=${DIR}/build
+
 LIBZMQ_DIR="${DIR}/libzmq-git"
-# CURRENT_DIR=`pwd`
 LIBZMQ_FILE="libzmq.a"
-# LIBZMQ_UNIVERSAL_FILE="libzmq.a"
-# LIBZMQ_UNIVERSAL_PREFIX="universal"
-# ZMQ_BUILD_LOG_FILE=$BUILD_DIR/build.log
+
 IOS_DEPLOY_TARGET="8.0"
 OSX_DEPLOY_TARGET="10.9"
-# VERSION_NUMBER="1.0.0"
 
 
-if [[ -f "${BUILD_DIR}/universal/lib/${LIBZMQ_FILE}" ]]; then
+if [[ -f "${DIR}/libzmq-ios.a" ]]; then
 exit 0;
 #library already built
 fi
@@ -36,12 +33,6 @@ if [[ -d ${BUILD_DIR} ]]; then
 rm -rf "${BUILD_DIR}"
 fi
 
-
-
-# mkdir -p "${BUILD_DIR}/armv7"
-# mkdir -p "${BUILD_DIR}/arm64"
-# mkdir -p "${BUILD_DIR}/i386"
-# better create them in setenv_all
 
 echo "Initializing dependency directory..."
 if [[ -d ${GLOBAL_OUTDIR} ]]; then
@@ -59,55 +50,34 @@ export ARCH=$1
 export SDK=$2
 export HOST=$3
 
-# export CC="$(xcrun -find -sdk ${SDK} cc)"
-# export CXX="$(xcrun -find -sdk ${SDK} cxx)"
-# export CPP="${CC} -E" # xcrun find cpp did not work
-
-# after an hour trying to figure what wasn't working with CPP, just realized it was CPPFLAGS, and is not necessary to export CC or CPP
 
 export SDKROOT
 SDKROOT=$(xcrun -sdk "${SDK}" --show-sdk-path)
-# export CFLAGS="-arch ${ARCH} -isysroot ${SDKROOT} -mios-version-min=${IOS_DEPLOY_TARGET}"
+
 export CFLAGS="${CFLAGS} -arch ${ARCH}"
 
 export CXXFLAGS="${CFLAGS}"
 export CPPFLAGS="${CFLAGS}"
 export LDFLAGS="${CFLAGS}"
 
-mkdir -p "${BUILD_DIR}/${ARCH}"
+mkdir -p "${BUILD_DIR}/${SDK}-${ARCH}"
 
 }
 
-setenv_armv7 ()
+
+
+
+setenv_ios ()
 {
 export CFLAGS="-mios-version-min=${IOS_DEPLOY_TARGET}"
-setenv "armv7" "iphoneos" "arm-apple-darwin"
+setenv $1 $2 $3
 }
 
-setenv_armv7s ()
-{
-export CFLAGS="-mios-version-min=${IOS_DEPLOY_TARGET}"
-setenv  "armv7s" "iphoneos" "arm-apple-darwin"
-}
-
-setenv_arm64 ()
-{
-export CFLAGS="-mios-version-min=${IOS_DEPLOY_TARGET}"
-setenv  "arm64" "iphoneos" "arm-apple-darwin"
-}
-
-setenv_i386 ()
+setenv_osx ()
 {
 export CFLAGS="-mmacosx-version-min=${OSX_DEPLOY_TARGET}"
-setenv "i386" "macosx" "i386-apple-darwin"
+setenv $1 $2 $3
 }
-
-setenv_x86_64 ()
-{
-export CFLAGS="-mmacosx-version-min=${OSX_DEPLOY_TARGET}"
-setenv "x86_64" "macosx" "x86_64-apple-darwin"
-}
-
 
 compile_zmq ()
 {
@@ -143,45 +113,83 @@ echo "running autogen.sh"
 "${LIBZMQ_DIR}"/autogen.sh
 
 
+echo "Compiling libzmq for iphoneos/iphonesimulator"
+echo "============================================="
+# ios and mac osx should be compiled in different files
+# https://karp.id.au/post/xcode_7_linker_rules/
+
 echo "Compiling libzmq for armv7..."
-setenv_armv7
+setenv_ios "armv7" "iphoneos" "arm-apple-darwin"
 compile_zmq
 
 echo "Compiling libzmq for armv7s..."
-setenv_armv7s
+setenv_ios "armv7s" "iphoneos" "arm-apple-darwin"
 compile_zmq
 
 echo "Compiling libzmq for arm64..."
-setenv_arm64
+setenv_ios "arm64" "iphoneos" "arm-apple-darwin"
 compile_zmq
 
 echo "Compiling libzmq for i386..."
-setenv_i386
+setenv_ios "i386" "iphonesimulator" "i386-apple-darwin"
 compile_zmq
 
 echo "Compiling libzmq for x86_64..."
-setenv_x86_64
+setenv_ios "x86_64" "iphonesimulator" "x86_64-apple-darwin"
 compile_zmq
 
 
-echo "Creating universal static library for armv7, armv7s, arm64, i386, x86_64"
+echo "Creating fat static library for iphoneos/iphonesimulator"
 
-mkdir -p "${BUILD_DIR}/${LIBZMQ_UNIVERSAL_PREFIX}/lib"
+lipo_input+=("${BUILD_DIR}/iphoneos-armv7/lib/${LIBZMQ_FILE}")
+lipo_input+=("${BUILD_DIR}/iphoneos-armv7s/lib/${LIBZMQ_FILE}")
+lipo_input+=("${BUILD_DIR}/iphoneos-arm64/lib/${LIBZMQ_FILE}")
+lipo_input+=("${BUILD_DIR}/iphonesimulator-i386/lib/${LIBZMQ_FILE}")
+lipo_input+=("${BUILD_DIR}/iphonesimulator-x86_64/lib/${LIBZMQ_FILE}")
 
-for arch in armv7 armv7s arm64 i386 x86_64 ; do
-lipo_input+=("${BUILD_DIR}/${arch}/lib/${LIBZMQ_FILE}")
-done
+mkdir -p "${BUILD_DIR}/universal-ios/"
+
+lipo -create ${lipo_input[*]} -output "${BUILD_DIR}/universal-ios/${LIBZMQ_FILE}"
 
 
-mkdir -p "${BUILD_DIR}/universal/lib/"
 
-lipo -create ${lipo_input[*]} -output "${BUILD_DIR}/universal/lib/${LIBZMQ_FILE}"
+echo "Compiling libzmq for macosx"
+echo "==========================="
+
+echo "Compiling libzmq for i386..."
+setenv_osx "i386" "macosx" "i386-apple-darwin"
+compile_zmq
+
+echo "Compiling libzmq for x86_64..."
+setenv_osx "x86_64" "macosx" "x86_64-apple-darwin"
+compile_zmq
+
+
+echo "Creating fat static library for macosx"
+
+lipo_input+=("${BUILD_DIR}/macosx-i386/lib/${LIBZMQ_FILE}")
+lipo_input+=("${BUILD_DIR}/macosx-x86_64/lib/${LIBZMQ_FILE}")
+
+mkdir -p "${BUILD_DIR}/universal-osx/"
+
+lipo -create ${lipo_input[*]} -output "${BUILD_DIR}/universal-osx/${LIBZMQ_FILE}"
+
+
+
+
+
 
 
 echo "Copying libzmq headers into universal library..."
 
-mkdir -p "${BUILD_DIR}/${LIBZMQ_UNIVERSAL_PREFIX}/include"
+mkdir -p "${BUILD_DIR}/universal"
 cp -R "${LIBZMQ_DIR}/include" "${BUILD_DIR}/universal"
+
+
+
+
+
+
 
 echo "Tidying up..."
 rm -rf builds
@@ -198,16 +206,8 @@ rm -rf packaging
 rm -rf tools
 
 
-cat << EOF
+cp ${DIR}/build/universal/include/zmq.h ${DIR}
+cp ${DIR}/build/universal/include/zmq_utils.h ${DIR}
+cp ${DIR}/build/universal-ios/libzmq.a ${DIR}/libzmq-ios.a
+cp ${DIR}/build/universal-osx/libzmq.a ${DIR}/libzmq-osx.a
 
-Finished compiling libzmq as a static library for iOS.
-
-Universal library can be found in build/universal;
-"lib" folder contains "libzmq.a" static library
-"include" folder contains headers
-
-To use in your project follow linking instructions
-available on the iOS zeromq page
-http://www.zeromq.org/build:iphone
-
-EOF
