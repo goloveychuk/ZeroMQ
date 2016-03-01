aaad844863d7990b7dbc97ce7a3ed18ecf5bdcc6
Rachel Brindle <you@subluminal.net>
2016-03-01 11:51:06 -0800

    Use Zewo's CZeroMQ instead of younata's

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftZMQ",
     dependencies: [
-        .Package(url: "https://github.com/younata/CZeroMQ.git", majorVersion: 1)
+        .Package(url: "https://github.com/Zewo/CZeroMQ.git", majorVersion: 1)
     ]
 )
