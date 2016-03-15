45b1088447e3f04645989c264bdc46ee2f689bf9
badim <goloveychuk@mail.ru>
2016-03-15 14:56:00 +0200

    migrate to data

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftZMQ",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/CZeroMQ.git", majorVersion: 1)
+        .Package(url: "https://github.com/Zewo/CZeroMQ.git", majorVersion: 1),
+        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 2)
     ]
 )
