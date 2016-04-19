92354c7fbacd8271d48758f85eaa72b958e913a0
goloveychuk <goloveychuk@users.noreply.github.com>
2016-04-19 21:37:20 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "ZeroMQ",
     targets: [Target(name: "ZeroMQ", dependencies: ["CZeroMQ"])],
     dependencies: [
-        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 5)
     ]
 )
