9312873f7d28f28f31ebeb99f07f71a29b459df1
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-26 23:19:02 -0300

    add tests

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "ZeroMQ",
-    targets: [Target(name: "ZeroMQ", dependencies: ["CZeroMQ"])],
     dependencies: [
-        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 5)
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/CZeroMQ.git", majorVersion: 0, minor: 5),
     ]
 )
---
+++ Tests/ZeroMQ/ZeroMQTests.swift
@@ -0,0 +1,36 @@
+import XCTest
+@testable import ZeroMQ
+
+class ZeroMQTests: XCTestCase {
+    func testPushPull() {
+        var called = false
+        do {
+            let context = try Context()
+
+            let inbound = try context.socket(.Pull)
+            try inbound.bind("tcp://127.0.0.1:5555")
+
+            let outbound = try context.socket(.Push)
+            try outbound.connect("tcp://127.0.0.1:5555")
+
+            try outbound.sendString("Hello World!")
+            try outbound.sendString("Bye!")
+
+            while let data = try inbound.receiveString() where data != "Bye!" {
+                called = true
+                XCTAssert(data == "Hello World!")
+            }
+        } catch {
+            XCTAssert(false)
+        }
+        XCTAssert(called)
+    }
+}
+
+extension ZeroMQTests {
+    static var allTests: [(String, ZeroMQTests -> () throws -> Void)] {
+        return [
+           ("testPushPull", testPushPull),
+        ]
+    }
+}
