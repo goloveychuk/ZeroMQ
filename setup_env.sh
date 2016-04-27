7cb1301d3db7bfe848c75c62015b2089951d2ed1
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-26 22:26:50 -0300

    update install script

--- setup_env.sh
+++ setup_env.sh
@@ -1,17 +1,15 @@
 
 #!/bin/bash
 
-if [[ "$(uname)" == "Darwin" ]]; then
-	brew update
-	brew install zeromq --with-libsodium --HEAD
-else
+if [[ "$(uname)" == "Linux" ]]; then
 	sudo apt-get update
 	sudo apt-get install build-essential pkg-config
-	cd /
-	sudo curl -O http://download.zeromq.org/zeromq-4.1.4.tar.gz
-	sudo tar xf /zeromq-4.1.4.tar.gz
-	cd /zeromq-4.1.4
-	sudo ./configure --without-libsodium
-	sudo make
-	sudo make install
 fi
+
+cd /tmp/
+sudo curl -O http://download.zeromq.org/zeromq-4.1.4.tar.gz
+sudo tar xf /tmp/zeromq-4.1.4.tar.gz
+cd /tmp/zeromq-4.1.4
+sudo ./configure --without-libsodium
+sudo make
+sudo make install
