e888bdd8f02fc8a5183add4de01c5663f825caf4
badim <goloveychuk@mail.ru>
2016-10-09 13:31:04 +0300

    removing zewo/core dependence

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "ZeroMQ",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/Core.git", majorVersion: 0, minor: 13),
         .Package(url: "https://github.com/Zewo/CZeroMQ.git", majorVersion: 1, minor: 0),
     ]
 )
