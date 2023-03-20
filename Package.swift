6dbeaa578d42c1fa39c0ed8a8644ae04967c2110
cryptoAlgorithm <cryptoalgro@gmail.com>
2023-03-20 19:04:40 +0800

    patch: update Package.swift for compatibility with Swift 5 SPM

--- Package.swift
+++ Package.swift
@@ -1,8 +1,10 @@
+// swift-tools-version: 5.7.0
+
 import PackageDescription
 
 let package = Package(
     name: "ZeroMQ",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/CZeroMQ.git", majorVersion: 1, minor: 0),
+        .package(url: "https://github.com/Zewo/CZeroMQ.git", branch: "master")
     ]
 )
